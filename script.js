c4018faa5606d55a1dc6c877a8d434d6e2100644
MegaEmme <mikmusco@gmail.com>
2025-03-13 14:35:23 +0100

    feat: solved main exercise

--- script.js
+++ script.js
@@ -1,2 +1,23 @@
 console.log("Mike's mail list");
 
+const mailBoolean = "https://flynn.boolean.careers/exercises/api/random/mail";
+console.log(mailBoolean);
+
+mailListElement = document.getElementById('mail-list');
+console.log(mailListElement);
+
+let mail = '';
+
+for (let i=0; i< 10; i++) {
+
+axios.get(mailBoolean).then((response) =>{
+
+    mail += `<div id="mail-list">${response.data.response}</div>`
+
+mailListElement.innerHTML = mail;
+})
+.catch(error => {
+    console.log('errore')
+})
+
+}
